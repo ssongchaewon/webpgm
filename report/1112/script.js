71e9aa61ae8c8238456a6d0e1eb0fa1b1476af14
songchaewon <gogogosong42@gmail.com>
2024-11-11 23:57:33 +0900

    Add files via upload

---
+++ report/1112/script.js
@@ -0,0 +1,15 @@
+const title = document.createElement("h1");
+title.textContent = "자기 소개 페이지";
+document.body.appendChild(title);
+
+const nameSection = document.createElement("p");
+nameSection.textContent = "이름: 송채원";  
+document.body.appendChild(nameSection);
+
+const hobbySection = document.createElement("p");
+hobbySection.textContent = "취미: 독서, 여행";
+document.body.appendChild(hobbySection);
+
+const introSection = document.createElement("p");
+introSection.textContent = "안녕하세요. 저는 게임 개발에 관심이 많은 사람입니다.";
+document.body.appendChild(introSection);
